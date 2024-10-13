12a5b188517b9c86f022f1ba6fa56cafa2d9f588
AnaGloria1 <145166017+AnaGloria1@users.noreply.github.com>
2024-10-13 14:40:00 -0600

    Update EcuacionPrimerGradoIntegrationTest.java

--- src/test/java/EcuacionPrimerGradoIntegrationTest.java
+++ src/test/java/EcuacionPrimerGradoIntegrationTest.java
@@ -1,1 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows; 
+import org.junit.jupiter.api.Test;
 
+public class EcuacionPrimerGradoIntegrationTest {
+    EcuacionPrimerGrado ecuacion = new EcuacionPrimerGrado();
+    @Test
+    public void solucionaEcuacionConMenos() throws Exception {
+        Double result = ecuacion.obtenerResultado("2x - 1 = 0");
+        Double valueExpected = 0.5; // Resultado esperado
+        assertEquals(valueExpected, result);
+    }
+
+    /**
+     * Prueba la solución de una ecuación con un término positivo en el lado izquierdo.
+     * Ecuación: 2x + 1 = 0
+     * Se espera que el resultado sea -0.5
+     */
+    @Test
+    public void solucionaEcuacionConMas() throws Exception {
+        Double result = ecuacion.obtenerResultado("2x + 1 = 0");
+        Double valueExpected = -0.5; // Resultado esperado
+        assertEquals(valueExpected, result);
+    }
